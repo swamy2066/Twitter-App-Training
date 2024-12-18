89b0dfa03d83fbc5bd1fd16a61f0f5946587109d
swamy2066 <abhi.sway93@gmail.com>
2024-12-18 16:48:06 +0530

    Latest changes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,115 +1,82 @@
-import React, { useRef, useState, useEffect, useContext } from 'react'
-import AuthContext from '../context/AuthProvider';
-import axios from '../service/axios';
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const LOGIN_URL = 'http://localhost:5000/users';
+
 const Login = () => {
-    const { setAuth } = useContext(AuthContext);
 
-    const userRef = useRef();
-    const errRef = useRef();
-
-    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+    const usenavigate = useNavigate();
 
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
-
-    useEffect(() => {
-        setErrMsg('');
-    }, [name, password])
+    const name = setUsername;
 
 
-    const handleSubmit = async (e) => {
+    const ProceedLogin = async (e) => {
         e.preventDefault();
+        let regobj = { username, password };
+        console.log(regobj);
 
-        try {
-
-            const response = await axios.get(LOGIN_URL, JSON.stringify({ name, password }),
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
-
-                });
-            console.log(JSON.stringify(response?.data));
-            const accessToken = response?.data?.accessToken;
-            const roles = response?.data?.roles;
-            setAuth({ name, password, roles, accessToken });
-            setName('');
-            setPassword('');
-            setSuccess(true);
-
-        } catch (err) {
-            if (!err?.response) {
-                setErrMsg('No Server Response')
-            } else if (err.response?.status === 400) {
-
-                setErrMsg('Missing Username or password');
-
-            } else if (err.response?.status === 401) {
-                setErrMsg('Unauthorized');
+        fetch("http://localhost:5000/users/" + name).then((res) => {
+            return res.json();
+        }).then((resp) => {
+            console.log(resp)
+            if (Object.keys(resp).length === 0) {
+                toast.error('Please Enter valid username');
             } else {
-                setErrMsg('Login Failed')
+                if (resp.password === password) {
+                    toast.success('Success');
+                    usenavigate('/')
+
+                } else {
+                    toast.error('Please Enter valid credentials');
+                }
+
             }
 
-            errRef.current.focus();
-        }
+        }).catch((err) => {
+            toast.error('Login Failed due to :' + err.message);
+        });
 
     }
 
 
-    return (
-        <>
-            {success ? (
-                <section>
-                    <h1>You are logged in !</h1>
-                    <br />
-                    <p>
-                        <a href="http://localhost:3000/">Go to Home</a>
-                    </p>
-                </section>
-            ) : (
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" :
-                        "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h1>Sign In</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setName(e.target.value)}
-                            value={name}
-                            required
-                        />
+     return (
 
-                        <label htmlFor="password">password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPassword(e.target.value)}
-                            value={password}
-                            required
-                        />
-                        <button disabled={!name || !password  ? true : false}>Sign In</button>
-                    </form>
-                    <p>
-                        Need an account? <br />
-                        <span className="line">
-                            {/*put router link here*/}
-                            <a href="http://localhost:3000/register">Sign Up</a>
-                        </span>
-                    </p>
-                </section>
-            )}
+        <section>
 
-        </>
+            <h1>Sign In</h1>
+            <form onSubmit={ProceedLogin}>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    autoComplete="off"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    required
+                />
+
+                <label htmlFor="password">password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                />
+                <button disabled={!username || !password ? true : false}>Sign In</button>
+            </form>
+            <p>
+                Need an account? <br />
+                <span className="line">
+
+                    <a href="http://localhost:3000/register">Sign Up</a>
+                </span>
+            </p>
+        </section>
+
     )
 }
 
